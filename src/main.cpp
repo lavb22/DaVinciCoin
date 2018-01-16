7c7c3731474ad3583a729327977058afcc2da84b
lavb22 <lavb22@gmail.com>
2018-01-16 09:16:30 -0400

    Nonce code added

--- src/main.cpp
+++ src/main.cpp
@@ -3270,7 +3270,7 @@ bool InitBlockIndex() {
         // Genesis block
         const char* pszTimestamp = "This is the genesis block. Davincicoins"; //genesis block message
         CTransaction txNew;
-        txNew.nTime = 1345083810;
+        txNew.nTime = 1516089600;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(9999) << vector<unsigned char>((const unsigned char*)pszTimestamp, (const unsigned char*)pszTimestamp + strlen(pszTimestamp));
@@ -3290,7 +3290,7 @@ bool InitBlockIndex() {
             block.nNonce   = 122894938;
         }
 
-//#ifdef TESTING
+#ifdef TESTING
         CBigNum bnTarget;
         bnTarget.SetCompact(block.nBits);
         while (block.GetHash() > bnTarget.getuint256())
@@ -3300,38 +3300,38 @@ bool InitBlockIndex() {
                        block.GetHash().ToString().c_str());
             block.nNonce++;
         }
-//#endif
+#endif
 
-//         	 //uncomment to log genesis block info
-//              //  start
-//                if (true && block.GetHash() != hashGenesisBlock)
-//                               {
-//                                   printf("Searching for genesis block...\n");
-//                                   uint256 hashTarget = CBigNum().SetCompact(block.nBits).getuint256();
-//                                   uint256 thash;
-//
-//                                   while (true)
-//                                   {
-//                                       thash = block.GetHash();
-//                                       if (thash <= hashTarget)
-//                                           break;
-//                                       if ((block.nNonce & 0xFFF) == 0)
-//                                       {
-//                                           printf("nonce %08X: hash = %s (target = %s)\n", block.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
-//                                       }
-//                                       ++block.nNonce;
-//                                       if (block.nNonce == 0)
-//                                       {
-//                                           printf("NONCE WRAPPED, incrementing time\n");
-//                                           ++block.nTime;
-//                                       }
-//                                   }
-//                                   printf("genesis.nTime = %u \n", block.nTime);
-//                                   printf("genesis.nNonce = %u \n", block.nNonce);
-//                                   printf("genesis.nVersion = %u \n", block.nVersion);
-//                               }
-//
-//                //end
+         	 //uncomment to log genesis block info
+              //  start
+                if (true && block.GetHash() != hashGenesisBlock)
+                               {
+                                   printf("Searching for genesis block...\n");
+                                   uint256 hashTarget = CBigNum().SetCompact(block.nBits).getuint256();
+                                   uint256 thash;
+
+                                   while (true)
+                                   {
+                                       thash = block.GetHash();
+                                       if (thash <= hashTarget)
+                                           break;
+                                       if ((block.nNonce & 0xFFF) == 0)
+                                       {
+                                           printf("nonce %08X: hash = %s (target = %s)\n", block.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
+                                       }
+                                       ++block.nNonce;
+                                       if (block.nNonce == 0)
+                                       {
+                                           printf("NONCE WRAPPED, incrementing time\n");
+                                           ++block.nTime;
+                                       }
+                                   }
+                                   printf("genesis.nTime = %u \n", block.nTime);
+                                   printf("genesis.nNonce = %u \n", block.nNonce);
+                                   printf("genesis.nVersion = %u \n", block.nVersion);
+                               }
+
+                //end
 
 
         //// debug print
