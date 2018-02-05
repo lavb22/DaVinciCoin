1199fbc5626c2cc0e1beb15404228117de3f58ed
Luis Valles <lavb22@gmail.com>
2018-02-05 13:28:59 -0400

    Changed net domains and client version

--- src/net.cpp
+++ src/net.cpp
@@ -1206,13 +1206,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"seeddavincicoin", "seed.davincicoin.net"},
-    {"seed", "seed.dcs.net"},
+    {"davincicoin.net", "seed.davincicoin.net"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"tseeddavincicoin", "tseed.davincicoin.net"},
+    {"davincicoin.net", "tseed.davincicoin.net"},
     {NULL, NULL}
 };
 
