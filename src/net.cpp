af02898c24b1dde49f21c58fe344a3a5884ff5bb
Luis Valles <lavb22@gmail.com>
2018-02-16 12:40:12 -0400

    Added seeder to net

--- src/net.cpp
+++ src/net.cpp
@@ -1206,12 +1206,12 @@ void MapPort(bool)
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strMainNetDNSSeed[][2] = {
-    {"davincicoin.net", "seed.davincicoin.net"},
+    {"seed.davincicoin.net", "seed.davincicoin.net"},
     {NULL, NULL}
 };
 
 static const char *strTestNetDNSSeed[][2] = {
-    {"davincicoin.net", "tseed.davincicoin.net"},
+    {"seeder.dcs1.cc", "seeder.dcs1.cc"},
     {NULL, NULL}
 };
 
